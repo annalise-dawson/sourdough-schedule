d30646c5d32b9d7403356e810d5363d28961bb4c
Annalise Dawson <annaliseamydawson@gmail.com>
2025-04-29 11:28:41 +1200

    Show the starter steps

--- client/components/Starter.tsx
+++ client/components/Starter.tsx
@@ -1,7 +1,24 @@
+import { useStarterSteps } from '../hooks/useStarterSteps'
+
 export default function Starter() {
+  const { data: steps, isLoading, error } = useStarterSteps()
+
+  if (isLoading) return <p>Preheating the oven...</p>
+  if (error) return <p>Bread is burning, be right back!</p>
+
   return (
-    <>
-      <h2>Still baking...</h2>
-    </>
+    <div className="bread-container">
+      <h2 className="bread-schedule-title">Starter Schedule</h2>
+      <div className="steps">
+        {steps.map((step) => (
+          <div key={step.id} className="step">
+            <p className="step-time">
+              <strong>Day {step.day}</strong>
+            </p>
+            <p className="step-instruction">{step.instructions}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
