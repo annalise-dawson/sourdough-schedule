7418027c4c1295f90207d8a4a10f7ae5ce3afeb0
Annalise Dawson <annaliseamydawson@gmail.com>
2025-04-28 13:34:00 +1200

    Change drop down times to 12 hour time

--- client/components/Bread.tsx
+++ client/components/Bread.tsx
@@ -3,18 +3,30 @@ import { Steps } from '../../models/steps'
 import { useState } from 'react'
 
 const times = [
-  { time: 1000 },
-  { time: 1100 },
-  { time: 1200 },
-  { time: 1300 },
-  { time: 1400 },
-  { time: 1500 },
-  { time: 1600 },
-  { time: 1700 },
-  { time: 1800 },
-  { time: 1900 },
-  { time: 2000 },
-  { time: 2100 },
+  { time: 1 },
+  { time: 2 },
+  { time: 3 },
+  { time: 4 },
+  { time: 5 },
+  { time: 6 },
+  { time: 7 },
+  { time: 8 },
+  { time: 9 },
+  { time: 10 },
+  { time: 11 },
+  { time: 12 },
+  { time: 13 },
+  { time: 14 },
+  { time: 15 },
+  { time: 16 },
+  { time: 17 },
+  { time: 18 },
+  { time: 19 },
+  { time: 20 },
+  { time: 21 },
+  { time: 22 },
+  { time: 23 },
+  { time: 24 },
 ]
 
 function add(num1: number, num2: number) {
@@ -23,13 +35,20 @@ function add(num1: number, num2: number) {
 
 export default function Bread() {
   const { data: steps, isLoading, error } = useSteps()
-  const [select, setSelect] = useState('0900')
+  const [select, setSelect] = useState(9)
 
   if (isLoading) return <p>Preheating the oven...</p>
   if (error) return <p>Bread is burning, be right back!</p>
 
   function handleTime(e: React.ChangeEvent<HTMLSelectElement>) {
     setSelect(e.target.value)
+  }
+
+  function convertTo12Hour(twentyfourhourtime: number) {
+    const amOrPm =
+      twentyfourhourtime === 24 ? 'am' : twentyfourhourtime >= 12 ? 'pm' : 'am'
+    const hour = twentyfourhourtime % 12 === 0 ? 12 : twentyfourhourtime % 12
+    return `${hour}${amOrPm}`
   }
 
   return (
@@ -45,12 +64,12 @@ export default function Bread() {
           onChange={handleTime}
           className="drop-down"
         >
-          <option value="0900" key="default">
-            0900
+          <option value="select time" key="default">
+            select time
           </option>
           {times.map((time) => (
             <option key={time.time} value={time.time}>
-              {time.time}
+              {convertTo12Hour(Number(time.time))}
             </option>
           ))}
         </select>
@@ -59,7 +78,7 @@ export default function Bread() {
         {steps.map((step: Steps) => (
           <div key={step.id} className="step">
             <p className="step-time">
-              <strong>{add(Number(select), step.setTime)}</strong>
+              <strong>{add(Number(select), step.setTime)}am</strong>
             </p>
             <p className="step-instruction">{step.instructions}</p>
           </div>
