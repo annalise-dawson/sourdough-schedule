eb40ffe1cd6c119050d6d3ab2d6ca766d8173347
Annalise Dawson <annaliseamydawson@gmail.com>
2025-08-09 21:28:04 +1200

    Add check boxes for steps

--- client/components/Starter.tsx
+++ client/components/Starter.tsx
@@ -1,11 +1,20 @@
 import { useStarterSteps } from '../hooks/useStarterSteps'
 import { StarterSteps } from '../../models/starter'
+import { useState } from 'react'
 
 export default function Starter() {
   const { data: steps, isLoading, error } = useStarterSteps()
+  const [isChecked, setIsChecked] = useState<{ [key: number]: boolean }>({})
 
   if (isLoading) return <p>Preheating the oven...</p>
   if (error) return <p>Bread is burning, be right back!</p>
+
+  function handleCheck(id: number) {
+    setIsChecked((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }))
+  }
 
   return (
     <div className="starter-page">
@@ -17,6 +26,13 @@ export default function Starter() {
               <strong>Day {step.day}</strong>
             </p>
             <p className="starter-step-instruction">{step.instructions}</p>
+            <input
+              type="checkbox"
+              checked={!!isChecked[step.id]}
+              onChange={() => handleCheck(step.id)}
+              className="bread-step-checkbox"
+            />
+            Step done!
           </div>
         ))}
       </div>
